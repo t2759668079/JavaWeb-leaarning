17f89cb45387e4589aa34bc97e08d5bf589d0de2
t2759668079 <2759668079@qq.com>
2024-09-20 16:28:48 +0800

    servlet 初始化

---
+++ class01-servlet/src/main/java/com/example/class01servlet/ServletDemo2.java
@@ -0,0 +1,59 @@
+package com.example.class01servlet;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class ServletDemo2 implements Servlet {
+    /**
+     * Servlet 初始化
+     * @param servletConfig
+     * @throws ServletException
+     */
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("ServletDemo2 初始化");
+
+    }
+
+    /**
+     * 获取Servlet配置
+     * @return
+     */
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    /**
+     * 执行Serlvet方法
+     * @param servletRequest
+     * @param servletResponse
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.getWriter().write("servletDemo2 执行");
+        System.out.println("Serlvet执行");
+    }
+
+    /**
+     * 获取serlvet信息
+     * @return
+     */
+    @Override
+    public String getServletInfo() {
+        return "";
+    }
+
+    /**
+     * 执行销毁方法
+     *
+     */
+    @Override
+    public void destroy() {
+        System.out.println("执行销毁方法");
+
+    }
+}
